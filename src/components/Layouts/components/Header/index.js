4f2de45e6db0f8c843792718b5eb5a697f87dc96
Linh Tran <tranlinh1542k@gmail.com>
2023-07-06 22:51:52 +0700

    building header logo, and search bar

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,6 +1,9 @@
 import classnames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import styles from './Header.module.scss';
+import images from '../../../../assets/images';
 
 const cx = classnames.bind(styles);
 
@@ -8,8 +11,25 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* logo */}
-                {/* search */}
+                {/* =========logo============= */}
+                <div className={cx('logo')}>
+                    <img src={images.logo.default} alt="tiktok-logo" />
+                </div>
+                {/* ==========search========== */}
+                <div className={cx('search')}>
+                    <input placeholder="search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear-btn')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
